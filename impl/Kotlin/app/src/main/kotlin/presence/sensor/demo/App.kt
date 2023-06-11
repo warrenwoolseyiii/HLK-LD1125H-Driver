80dc66c81142b80e3f8ca5476858ee9a81aaddf1
budgettsfrog <budgettsfrog@protonmail.com>
2023-06-11 09:27:46 -0700

    Able to open the pipe and read data - not able to exit the program I want to yet - also doesn't handle the service shut down properly

--- impl/Kotlin/app/src/main/kotlin/presence/sensor/demo/App.kt
+++ impl/Kotlin/app/src/main/kotlin/presence/sensor/demo/App.kt
@@ -1,13 +1,16 @@
-import java.io.File
-import java.util.concurrent.TimeUnit
+package presence.sensor.demo
+
 import kotlinx.serialization.json.*
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
+import java.util.concurrent.TimeUnit
+import java.util.Scanner
 
 fun String.toJSONObject() = Json.parseToJsonElement(this).jsonObject
-fun JsonObject.getDouble(key: String) = this[key]!!.jsonPrimitive.content.toDouble()
+fun JsonObject.getDouble(key: String) = this[key]?.jsonPrimitive?.doubleOrNull
 
 fun main() {
-    val pipePath = "/path/to/your/named/pipe" // replace with your named pipe path
-
     if (!isServiceRunning()) {
         if (!startService()) {
             println("Failed to start the service. Exiting.")
@@ -15,31 +18,50 @@ fun main() {
         }
     }
 
-    val pipeFile = File(pipePath)
-    val threshold = 10.0 // replace with your desired threshold value
+    val pipeFile = File(Settings.PIPE_PATH)
 
-    pipeFile.bufferedReader().useLines { lines ->
-        lines.forEach { line ->
-            val json = line.toJSONObject()
-            val distance = json.getDouble("dist")
-            if (distance < threshold) {
-                println("Someone is present")
-            } else {
-                println("No one is present")
+    val readerThread = Thread {
+        while(pipeFile.exists()) {
+            pipeFile.bufferedReader().useLines { lines ->
+                lines.forEach { line ->
+                    /*val json = line.toJSONObject()
+                    val distance = json.getDouble("dist")
+                    if (distance != null && distance < Settings.THRESHOLD) {
+                        println("Someone is present")
+                    } else {
+                        println("No one is present")
+                    }*/
+                    // Just print the raw line
+                    println(line)
+                }
             }
+            Thread.sleep(10)
         }
     }
+
+    readerThread.start()
+
+    val scanner = Scanner(System.`in`)
+    while (scanner.hasNextLine()) {
+        val line = scanner.nextLine()
+        if (line.equals("X", true)) {
+            readerThread.interrupt()
+            break
+        }
+    }
+
+    scanner.close()
 }
 
 fun isServiceRunning(): Boolean {
-    val cmd = "pgrep -f sensor_service" // replace 'sensor_service' with the actual name of your service
+    val cmd = "pgrep -f ${Settings.SERVICE_NAME}"
     val proc = Runtime.getRuntime().exec(cmd)
     proc.waitFor(5, TimeUnit.SECONDS)
     return proc.exitValue() == 0
 }
 
 fun startService(): Boolean {
-    val cmd = "/path/to/sensor_service &" // replace with the actual path to your service
+    val cmd = "${Settings.SERVICE_PATH} &"
     val proc = Runtime.getRuntime().exec(cmd)
     proc.waitFor(5, TimeUnit.SECONDS)
     return proc.exitValue() == 0
---
+++ impl/Kotlin/app/src/main/kotlin/presence/sensor/demo/Settings.kt
@@ -0,0 +1,8 @@
+package presence.sensor.demo
+
+object Settings {
+    const val SERVICE_NAME = "hlk_ld1125h" // replace 'sensor_service' with the actual name of your service
+    const val PIPE_PATH = "/home/budgettsfrog/presence_data.fifo" // replace with your named pipe path
+    const val SERVICE_PATH = "/mnt/c/Users/budge/Repos/HLK-LD1125H-Driver/impl/C/build/hlk_ld1125h" // replace with the actual path to your service
+    const val THRESHOLD = 1.0 // replace with your desired threshold value
+}
